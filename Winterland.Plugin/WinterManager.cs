5540df5d137821e15f554e7d1ef1f7d08fb8e35f
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-11-12 18:43:12 -0300

    plugin boilerplate cause i love boilerplate.

---
+++ Winterland.Plugin/WinterManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Winterland.Plugin
+{
+    public class WinterManager : MonoBehaviour
+    {
+        public static WinterManager Instance
+        {
+            get;
+            private set;
+        }
+
+        public static WinterManager Create()
+        {
+            var gameObject = new GameObject("Winter Manager");
+            return gameObject.AddComponent<WinterManager>();
+        }
+
+        void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
---
+++ Winterland.Plugin/WinterlandPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using System;
+using Reptile;
+
+namespace Winterland.Plugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class WinterlandPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            try
+            {
+                StageManager.OnStagePostInitialization += StageManager_OnStagePostInitialization;
+                Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} is loaded!");
+            }
+            catch(Exception e)
+            {
+                Logger.LogError($"Plugin {PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} failed to load!{Environment.NewLine}{e}");
+            }
+        }
+
+        private void StageManager_OnStagePostInitialization()
+        {
+            if (Core.Instance.BaseModule.CurrentStage != Stage.square)
+                return;
+            WinterManager.Create();
+        }
+    }
+}
