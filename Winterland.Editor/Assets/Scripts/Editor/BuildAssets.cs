b70ab06015a37bc0ec910c62623050b9a83359c5
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-11-13 23:49:50 -0300

    Set up copying assetbundles and dll outputs to plugin folder automatically

--- Winterland.Editor/Assets/Scripts/Editor/BuildAssets.cs
+++ Winterland.Editor/Assets/Scripts/Editor/BuildAssets.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.IO;
 using UnityEditor;
@@ -5,7 +6,8 @@ using UnityEditor.SceneManagement;
 
 public class BuildAssets {
 
-    private const string OutputDirectory = "Build";
+    private const string OutputDirectory = "AssetBundles";
+    private const string PluginName = "MilleniumWinterland";
 
     [MenuItem("BRC/Build Assets")]
     private static void BuildAllAssetBundles() {
@@ -13,8 +15,54 @@ public class BuildAssets {
         CleanUpOutputDirectoryPreBuild(OutputDirectory);
         BuildAssetBundles(OutputDirectory);
         CleanUpOutputDirectoryPostBuild(OutputDirectory);
-        if (Directory.Exists(OutputDirectory))
-            Process.Start(OutputDirectory);
+        CopyToBepInExPluginsFolder(OutputDirectory, PluginName);
+    }
+
+    private static void CopyToBepInExPluginsFolder(string outputDirectory, string pluginName) {
+        var bepinexDirectory = Environment.GetEnvironmentVariable("BepInExDirectory", EnvironmentVariableTarget.User);
+        if (bepinexDirectory == null)
+            bepinexDirectory = Environment.GetEnvironmentVariable("BepInExDirectory", EnvironmentVariableTarget.Machine);
+        if (bepinexDirectory == null) {
+            UnityEngine.Debug.LogWarning($"Please set your BepInExDirectory Environment variable on your system to your BepInEx path for the asset bundles to be automatically copied to the plugin folder.");
+            return;
+        }
+        var pathToPlugin = Path.Combine(bepinexDirectory, "plugins", pluginName);
+        var pathToBundles = Path.Combine(pathToPlugin, outputDirectory);
+        if (Directory.Exists(pathToBundles))
+            Directory.Delete(pathToBundles, true);
+        if (!Directory.Exists(pathToBundles))
+            Directory.CreateDirectory(pathToBundles);
+        CopyDirectory(outputDirectory, pathToBundles, true);
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+    private static void CopyDirectory(string sourceDir, string destinationDir, bool recursive) {
+        // Get information about the source directory
+        var dir = new DirectoryInfo(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+        // Cache directories before we start copying
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (FileInfo file in dir.GetFiles()) {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (recursive) {
+            foreach (DirectoryInfo subDir in dirs) {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir, true);
+            }
+        }
     }
 
     private static void PreBuildAssetBundles() {
