3fb1ea120d58f84e7c6b0061908bf3483acc26b7
Andrew Bradley <cspotcode@gmail.com>
2023-11-18 13:37:51 -0500

    Conditionally disable F5 hotkey in winterland project since it conflicts w/winterland build

--- Assets/Do Not Touch/Scripts/Editor/Hotkeys.cs
+++ Assets/Do Not Touch/Scripts/Editor/Hotkeys.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 /// global hotkey bindings, logic should live elsewhere
 /// </summary>
 static class Hotkeys {
+    #if !WINTERLAND
+    // Disable binding for winterland project
     [Shortcut(Constants.hotkeyGroupName + "/Rebuild Map", KeyCode.F5)]
+    #endif
     static void RebuildAll() {
         new BRCMapBuilder(Object.FindFirstObjectByType<BRCMap>()).RebuildAll();
     }
