ae841d4b1945ab10dad38ab4d6b23ec44bb300e9
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-11-20 14:54:29 -0300

    Update rebuild.ps1

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -1,14 +1,4 @@
 cd ../
+Write-Output "====== Building Assemblies ======="
 
-Write-Output "====== Building Common Scripts ======="
-
-cd Winterland.Common
 dotnet build
-
-Write-Output "====== Building Plugin ======="
-
-cd ../
-cd Winterland.Plugin
-dotnet build
-
-cd ../
