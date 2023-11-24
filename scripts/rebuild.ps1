e90e918179f161e0e8a8f72a553de9afd47c66b4
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-11-24 19:07:18 -0300

    restore guids regardless of existence

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -1,22 +1,15 @@
 cd ../
 Write-Output "====== Restoring Assembly GUIDs ======="
+
 $metaPath = ".\Winterland.Editor\Assets\Scripts\Winterland.Common.dll.meta"
-$metaExists = Test-Path $metaPath
-if (-not $metaExists){
-    git checkout $metaPath
-}
+git checkout $metaPath
 
 $metaPath = ".\Winterland.Editor\Assets\Scripts\EasyDecal.Runtime.dll.meta"
-$metaExists = Test-Path $metaPath
-if (-not $metaExists){
-    git checkout $metaPath
-}
+git checkout $metaPath
 
 $metaPath = ".\Winterland.Editor\Assets\Scripts\Winterland.MapStation.Common.dll.meta"
-$metaExists = Test-Path $metaPath
-if (-not $metaExists){
-    git checkout $metaPath
-}
+git checkout $metaPath
+
 Write-Output "====== Building Assemblies ======="
 
 dotnet build
