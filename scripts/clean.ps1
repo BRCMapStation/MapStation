990caef2d54b8cc856e726f8acb849dc1069bbc8
Andrew Bradley <cspotcode@gmail.com>
2023-11-25 18:18:54 -0500

    add clean script that removes everything.  `dotnet clean` leaves our .mdb and some other files behind

---
+++ scripts/clean.ps1
@@ -0,0 +1,19 @@
+Set-Location $PSScriptRoot/..
+$ErrorActionPreference = 'Stop'
+
+# `dotnet clean` doesn't delete everything.
+
+Write-Output `
+    .\libs\BRC-CommonAPI\bin `
+    .\libs\BRC-CommonAPI\obj `
+    .\Winterland.MapStation.Common\bin `
+    .\Winterland.MapStation.Common\obj `
+    .\Winterland.MapStation.Plugin\bin `
+    .\Winterland.MapStation.Plugin\obj `
+    .\Winterland.Common\obj `
+    .\Winterland.Common\bin `
+    .\Winterland.Plugin\obj `
+    .\Winterland.Plugin\bin `
+| ForEach-Object {
+    if(test-path $_) { remove-item -recurse $_ }
+}
