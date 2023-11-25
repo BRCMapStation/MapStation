09782e5ee2dcd31de9b6b63556cf3d5ba718b12f
Andrew Bradley <cspotcode@gmail.com>
2023-11-25 18:00:24 -0500

    Suppress warning CS0436 from generated AssemblyInfo.cs Add support for attaching debugger to game - Add script to convert your game to a debug build - In "Debug" configuration, build will convert pdb to mdb debug symbols Remove "directory already exists" output from build log; xcopy /i already creates directory

---
+++ scripts/convert-to-debug-build.ps1
@@ -0,0 +1,26 @@
+param($brcInstallDirectory, $unityEditorDirectory)
+
+if(-not $unityEditorDirectory) {
+    $unityEditorDirectory="C:\Program Files\Unity\Hub\Editor\2021.3.27f1\Editor\Data\PlaybackEngines\windowsstandalonesupport\Variations\win64_player_development_mono"
+}
+if(-not $brcInstallDirectory) {
+    $brcInstallDirectory=$env:BRCPath
+}
+
+Write-Host "This script will convert your Bomb Rush Cyberfunk installation into a debug build."
+Write-Host "Backup your game before proceeding!  This may break things."
+Write-Host ""
+Write-Host "Bomb Rush Cyberfunk installation:  $brcInstallDirectory"
+Write-Host "Unity Editor:                      $unityEditorDirectory"
+Write-Host ""
+
+$confirmation = Read-Host "Are you sure? (type 'yes')"
+if($confirmation -ne "yes") {
+    Write-Host "Aborting"
+    exit
+}
+
+xcopy /E /C /Y "$unityEditorDirectory\Data" "$brcInstallDirectory\Bomb Rush Cyberfunk_data"
+xcopy /C /Y "$unityEditorDirectory\UnityPlayer.dll" "$brcInstallDirectory\UnityPlayer.dll"
+xcopy /C /Y "$unityEditorDirectory\WindowsPlayer.exe" "$brcInstallDirectory\Bomb Rush Cyberfunk.exe"
+Add-Content -Path "$brcInstallDirectory\Bomb Rush Cyberfunk_data\boot.config" "`n`nplayer-connection-debug=1"
