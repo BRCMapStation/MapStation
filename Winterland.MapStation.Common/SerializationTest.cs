8a36ad6fadbb268889d8e75e52e8f533f8394e2e
Andrew Bradley <cspotcode@gmail.com>
2023-11-26 16:49:29 -0500

    add component to test deserialization of plugin structs and classes

---
+++ Winterland.MapStation.Common/SerializationTest.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace Winterland.MapStation.Common {
+    public class SerializationTest : MonoBehaviour {
+        public SerializedStruct data = new SerializedStruct();
+        [Serializable]
+        public struct SerializedStruct {
+            public string Foo;
+            public string Bar;
+        }
+
+        private void Awake() {
+            Debug.Log(String.Format("Serialized data: foo={0}, bar={1}", data.Foo, data.Bar));
+        }
+    }
+
+}
