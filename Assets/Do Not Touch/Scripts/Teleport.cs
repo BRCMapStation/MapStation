6214ec635fa72cd1040099f6e5471055a6ed3126
Andrew Bradley <cspotcode@gmail.com>
2023-11-26 16:57:30 -0500

    Update Teleport code, add door-style teleporter prefab

--- Assets/Do Not Touch/Scripts/Teleport.cs
+++ Assets/Do Not Touch/Scripts/Teleport.cs
@@ -5,53 +5,16 @@ namespace Reptile
 {
 	public class Teleport : MonoBehaviour
 	{
-		private Coroutine teleportRoutine;
-
 		[Header("In case of a teleport, it will place you at the spawner of that teleport")]
 		[Header("You can put a teleport OR a spawner in here, both work")]
 		public Transform teleportTo;
 
-		public bool giveSpeedAtSpawn = false;
+		public bool giveSpeedAtSpawn = true;
 
 		public bool letAIGoToTheNextWaypoint;
 
 		public bool automaticallyReturnPlayerToLastSafeLocation;
 
 		public bool doDamage;
-
-		private float fadeToBlackDurationDeathzone = 0.5f;
-
-		private float blackDurationDeathzone = 1.75f;
-
-		private float fadeOpenDurationDeathzone = 4.5f;
-
-		private float fadeToBlackDurationDoor = 0.5f;
-
-		private float blackDurationDoor = 0.8f;
-
-		private float fadeOpenDurationDoor = 1.6f;
-
-		private float fadeToBlackDuration;
-
-		private float blackDuration;
-
-		private float fadeOpenDuration;
-
-		private int runHash = Animator.StringToHash("run");
-
-		private void OnEnable()
-		{
-			teleportRoutine = null;
-
-			// Suppress "is assigned but its value is never used" in Unity Editor
-			var a = fadeToBlackDurationDoor;
-			var b = teleportRoutine;
-			var c = blackDurationDoor;
-			var d = fadeOpenDurationDoor;
-			var e = blackDurationDeathzone;
-			var f = fadeOpenDurationDeathzone;
-			var g = fadeToBlackDurationDeathzone;
-			var h = giveSpeedAtSpawn;
-		}
 	}
 }
