de6e432cd0ac0d44c19ec5d9d9aad9e41928321a
Andrew Bradley <cspotcode@gmail.com>
2023-11-26 19:14:09 -0500

    Give prefabs consistent naming, add them to right-click menu - undo is disabled for some because undoing them was crashing unity editor!

---
+++ Assets/Do Not Touch/Scripts/Editor/PrefabContextMenu.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using Winterland.Common;
+public class AddPrefabsToContextMenu {
+    private const int Priority = -30;
+    private const string PrefabPathPrefix = "Assets/MappingToolkit/Assets/Map Template/";
+
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Door Teleport", priority = Priority)]
+    private static void CreateTeleport(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "DoorTeleporterPrefab");
+    }
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Out-of-Bounds Teleport", priority = Priority)]
+    private static void CreateOobTeleport(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "TeleporterPrefab");
+    }
+
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Small Graffiti Spot", priority = Priority)]
+    private static void CreateGraffitiSpotSmall(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "GraffitiSpotSmallPrefab", false);
+    }
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Medium Graffiti Spot", priority = Priority)]
+    private static void CreateGraffitiSpotMedium(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "GraffitiSpotMediumPrefab", false);
+    }
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Large Graffiti Spot", priority = Priority)]
+    private static void CreateGraffitiSpotLarge(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "GraffitiSpotLargePrefab", false);
+    }
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Extra Large Graffiti Spot", priority = Priority)]
+    private static void CreateGraffitiSpotExtraLarge(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "GraffitiSpotExtraLargePrefab", false);
+    }
+
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Grind", priority = Priority)]
+    private static void CreateGrind(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "GrindPrefab", false);
+    }
+
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Vert Ramp", priority = Priority)]
+    private static void CreateVertRamp(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "VertRampPrefab");
+    }
+    [MenuItem("GameObject/" + Constants.menuLabel + "/Create Wallrun", priority = Priority)]
+    private static void CreateWallrun(MenuCommand menuCommand) {
+        CreatePrefabUnderContext(menuCommand.context, "WallRunPrefab");
+    }
+
+    private static void CreatePrefabUnderContext(Object context, string PrefabName, bool supportUndo = true) {
+        var assetPath = PrefabPathPrefix + PrefabName + ".prefab";
+        var prefabAsset = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+        if(prefabAsset == null) {
+            Debug.LogError(string.Format("Prefab not found at path {0}", assetPath));
+            return;
+        }
+        var prefabInstance = PrefabUtility.InstantiatePrefab(prefabAsset) as GameObject;
+        StageUtility.PlaceGameObjectInCurrentStage(prefabInstance);
+        GameObjectUtility.SetParentAndAlign(prefabInstance, context as GameObject);
+        prefabInstance.transform.position = SceneView.lastActiveSceneView.pivot;
+        // Undo for some prefabs is *crashing* Unity Editor.
+        // Broken/misbehaving Undo is better than losing your work.
+        if(supportUndo) {
+            Undo.RegisterCreatedObjectUndo(prefabInstance, $"Create {prefabInstance.name}");
+        }
+        Selection.activeObject = prefabInstance;
+    }
+}
