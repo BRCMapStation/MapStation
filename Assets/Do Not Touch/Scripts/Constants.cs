f53c7c6062e1f7bc8f37da6a2bd0a5f256b7a6cd
cspotcode <cspotcode@gmail.com>
2023-11-30 16:09:34 -0500

    Fix missing const

--- Assets/Do Not Touch/Scripts/Constants.cs
+++ Assets/Do Not Touch/Scripts/Constants.cs
@@ -1,5 +1,6 @@
 public static class Constants {
     public const string hotkeyGroupName = "BRC";
     public const string menuLabel = "BRC Mapping Toolkit";
+    public const string experimentsSubmenuLabel = "Experiments";
     public const string GizmoIconBaseDir = "Assets/MappingToolkit/Assets/Do Not Touch/Icons";
 }
