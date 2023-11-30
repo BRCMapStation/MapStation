de933346641a4a105854f2474993239ba7358281
cspotcode <cspotcode@gmail.com>
2023-11-30 17:17:59 -0500

    Add tool to search for MonoBehaviour GUIDs and localIds

---
+++ Assets/Do Not Touch/Scripts/Editor/ComponentGuidFinderWindow.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Scene = UnityEngine.SceneManagement.Scene;
+
+public class ComponentGuidFinderWindow : EditorWindow {
+    const string windowLabel = "Component Guid Finder";
+
+    [MenuItem(Constants.menuLabel + "/" + Constants.experimentsSubmenuLabel + "/" + windowLabel)]
+    private static void ShowMyEditor() {
+        EditorWindow wnd = GetWindow<ComponentGuidFinderWindow>();
+        wnd.titleContent = new GUIContent(windowLabel);
+    }
+
+    private Vector2 scrollbarPosition = Vector2.zero;
+
+    private string ComponentName;
+    private string Output = "";
+    private void OnGUI() {
+        EditorGUILayout.HelpBox(
+            "Enter the name of a MonoBehaviour and click Search.\n" +
+            "Output will show yaml-formatted GUID and localId pairs for the component.\n\n" +
+            "This may be helpful when fixing missing scripts or find-and-replacing DLL " +
+            "components to point to their Package Manager counterparts.",
+            MessageType.None);
+        GUILayout.Space(10);
+
+        ComponentName = EditorGUILayout.TextField(ComponentName);
+
+        if(GUILayout.Button("Search")) {
+            Output = "";
+            foreach(var path in AssetDatabase.GetAllAssetPaths()) {
+                if(!path.EndsWith(".cs") && !path.EndsWith(".dll")) continue;
+                foreach(var asset in AssetDatabase.LoadAllAssetsAtPath(path)) {
+                    if(asset is MonoScript script) {
+                        if(asset.name == ComponentName) {
+                            AssetDatabase.TryGetGUIDAndLocalFileIdentifier(asset, out var Guid, out long localId);
+                            Output += string.Format("m_Script: {{fileID: {0}, guid: {1}, type: 3}} # {2}\n", localId, Guid, script.name);
+                        }
+                    }
+                }
+            }
+        }
+
+        GUILayout.Space(10);
+        GUILayout.Label("Results");
+        EditorGUILayout.SelectableLabel(Output, EditorStyles.textArea);
+    }
+}
