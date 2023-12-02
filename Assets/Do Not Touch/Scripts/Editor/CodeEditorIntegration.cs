75841dd241cf65cc5f8239213b9d1497e8da9159
Andrew Bradley <cspotcode@gmail.com>
2023-12-02 17:52:40 -0500

    Add experimental option to modify emitted .csproj files for compat w/VSCode and our root .sln

---
+++ Assets/Do Not Touch/Scripts/Editor/CodeEditorIntegration.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class CsprojPostprocessorWindow : EditorWindow {
+    const string WindowLabel = "Csproj Postprocessor";
+
+    [MenuItem(Constants.menuLabel + "/" + Constants.experimentsSubmenuLabel + "/" + WindowLabel, priority = Constants.experimentsSubmenuPriority)]
+    private static void ShowMyEditor() {
+        EditorWindow wnd = GetWindow<CsprojPostprocessorWindow>();
+        wnd.titleContent = new GUIContent(WindowLabel);
+    }
+
+    private bool applyModifications_ = false;
+
+    private void OnEnable() {
+        // On domain reload, restore static state from serialized instance state
+        CSProjPostprocessor.applyModifications = applyModifications_;
+    }
+
+    private void OnGUI() {
+        CSProjPostprocessor.applyModifications = applyModifications_ = GUILayout.Toggle(applyModifications_, "Apply modifications");
+    }
+}
+
+public class CSProjPostprocessor : AssetPostprocessor
+{
+    internal static bool applyModifications = false;
+
+    private static string OnGeneratedCSProject(string path, string contents) {
+        EditorGUILayout.HelpBox(
+            "Tweaks .csproj files emitted by Unity, for compatibility with VSCode.\n" + 
+            "Or maybe cspotcode doesn't know what he's doing.",
+            MessageType.Info
+        );
+        if(applyModifications) {
+            Debug.LogFormat("Generating {0}", path);
+            contents = contents.Replace("<ProjectCapability Remove=\"AssemblyReferences\" />", "<!-- <ProjectCapability Remove=\"AssemblyReferences\" /> -->");
+            contents = contents.Replace("<TargetFramework>netstandard2.1</TargetFramework>", "<TargetFramework>net46</TargetFramework>");
+            contents = contents.Replace("<LangVersion>9.0</LangVersion>", "<LangVersion>latest</LangVersion>");
+        }
+        return contents;
+    }
+}
