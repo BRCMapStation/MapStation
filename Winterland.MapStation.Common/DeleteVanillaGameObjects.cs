8ba3269d17fa56b9fc4d0c9a6389b8a016f2945f
Andrew Bradley <cspotcode@gmail.com>
2023-12-03 13:02:03 -0500

    Replace vanilla FailSafeTrigger with our own we can move downward to clear space for hidden rooms

---
+++ Winterland.MapStation.Common/DeleteVanillaGameObjects.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Winterland.MapStation.Common.Serialization;
+
+namespace Winterland.MapStation.Common.VanillaAssets {
+    /// <summary>
+    /// This component deletes GameObjects by path, allowing us to remove things from the base map.
+    /// We can also replace things from the base map by deleting them and shipping a replacement in our prefab.
+    /// </summary>
+    public class DeleteVanillaGameObjectsV1 : MonoBehaviour {
+
+        // BepInEx serialization workaround
+        private List<Deletion> Deletions => deletions.items;
+        [SerializeReference] private SList_Deletion deletions = new ();
+        public class SList_Deletion : SList<Deletion> {}
+
+        private void Awake() {
+            DeleteGameObjects();
+        }
+
+        public void DeleteGameObjects() {
+            foreach(var d in Deletions) {
+                var go = GameObject.Find(d.Path);
+                if(go == null) {
+                    Debug.Log($"{nameof(DeleteVanillaGameObjectsV1)} could not find GameObject to delete at path: {d.Path}");
+                } else {
+                    Destroy(go);
+                }
+            }
+        }
+        [Serializable]
+        public class Deletion {
+            public string Path;
+        }
+    }
+}
