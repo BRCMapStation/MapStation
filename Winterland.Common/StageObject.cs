a98b703cdd19892ae116bbe3ce278f657032a40c
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-05 19:29:49 -0300

    implement chunks

--- Winterland.Common/StageObject.cs
+++ Winterland.Common/StageObject.cs
@@ -7,12 +7,32 @@ using UnityEngine;
 using Reptile;
 
 namespace Winterland.Common {
+    /// <summary>
+    /// This object will be automatically put into a StageChunk. Useful for components that depend on chunks, like Junk.
+    /// </summary>
     public class StageObject : MonoBehaviour {
+        [Header("Name of the chunk GameObject. Leave this empty to automatically find a chunk.")]
+        [SerializeField]
+        private string chunkName = "";
         public void PutInChunk() {
+            var myChunk = GetComponentInParent<StageChunk>();
+            if (myChunk != null)
+                return;
+            var parentStageObject = GetComponentInParent<StageObject>();
+            if (parentStageObject != this) {
+                parentStageObject.PutInChunk();
+                return;
+            }
             var chunks = FindObjectsOfType<StageChunk>(true);
             foreach(var chunk in chunks) {
                 if (chunk == null)
                     continue;
+                if (!string.IsNullOrEmpty(chunkName)) {
+                    if (chunk.gameObject.name == chunkName)
+                        transform.SetParent(chunk.transform);
+                    else
+                        continue;
+                }
                 var colliders = chunk.GetComponentsInChildren<BoxCollider>(true);
                 foreach(var chunkCollider in colliders) {
                     if (chunkCollider == null)
---
+++ Winterland.Editor/Assets/Scripts/StageChunk.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stub class for custom chunks.
+/// </summary>
+public class StageChunk : MonoBehaviour
+{
+}
