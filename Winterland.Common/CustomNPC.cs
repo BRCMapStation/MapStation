519d10b7ace1bfdefdbf7b69c86491199fd4846d
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-06 04:32:27 -0300

    start custom npc

---
+++ Winterland.Common/CustomNPC.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using CommonAPI;
+
+namespace Winterland.Common {
+    public class CustomNPC : MonoBehaviour {
+        public string Name = "";
+
+        private void Awake() {
+
+        }
+    }
+}
