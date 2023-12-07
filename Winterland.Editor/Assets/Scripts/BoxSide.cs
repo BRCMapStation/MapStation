a0ae6bfd2c8eb697f5fd360b9036031f62745720
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-07 12:22:19 -0300

    add camera zones

---
+++ Winterland.Editor/Assets/Scripts/BoxSide.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Reptile {
+    // Token: 0x020002DF RID: 735
+    public enum BoxSide {
+        // Token: 0x04001B39 RID: 6969
+        FORWARD,
+        // Token: 0x04001B3A RID: 6970
+        BACK,
+        // Token: 0x04001B3B RID: 6971
+        RIGHT,
+        // Token: 0x04001B3C RID: 6972
+        LEFT,
+        // Token: 0x04001B3D RID: 6973
+        DOWN,
+        // Token: 0x04001B3E RID: 6974
+        UP,
+        // Token: 0x04001B3F RID: 6975
+        MAX
+    }
+}
