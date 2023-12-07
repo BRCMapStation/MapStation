c112ea25c39633df17d4800be2665835d45e50d8
Andrew Bradley <cspotcode@gmail.com>
2023-12-06 22:41:18 -0500

    cleanup

--- Winterland.Plugin/Patches/PlayerPatch.cs
+++ Winterland.Plugin/Patches/PlayerPatch.cs
@@ -15,7 +15,7 @@ namespace Winterland.Plugin.Patches {
         private static void Init_Postfix(Player __instance) {
             var winterPlayer = __instance.gameObject.AddComponent<WinterPlayer>();
             winterPlayer.player = __instance;
-            if(WinterConfig.Instance.DisableKBMInput.Value) {
+            if(WinterConfig.Instance.DisableKBMInputValue) {
                 KBMInputDisabler.Disable();
             }
         }
