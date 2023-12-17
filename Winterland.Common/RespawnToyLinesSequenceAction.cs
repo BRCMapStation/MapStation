c850bb8aefa0d8116ddd7359f8e548353b9dc29f
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-16 23:34:13 -0300

    fix typo, wait for server response for toy line collection.

--- Winterland.Common/RespawnToyLinesSequenceAction.cs
+++ Winterland.Common/RespawnToyLinesSequenceAction.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using UnityEngine;
 using SlopCrew.API;
 using SlopCrew.Server.XmasEvent;
+using CommonAPI;
+using Reptile;
+using System.Collections;
 
 namespace Winterland.Common {
     public class RespawnToyLinesSequenceAction : SequenceAction {
@@ -15,20 +18,48 @@ namespace Winterland.Common {
         public string RejectedTarget = "";
         [Header("Name of action to jump to if can't reach SlopCrew")]
         public string NoConnectionTarget = "";
+        private bool immediate;
+        private const float TimeOutSeconds = 5f;
         public override void Run(bool immediate) {
             base.Run(immediate);
+            this.immediate = immediate;
+            var sequenceHandler = CustomSequenceHandler.instance;
             var api = APIManager.API;
             if (api.Connected) {
+                sequenceHandler.skipTextActiveState = SequenceHandler.SkipState.NOT_SKIPPABLE;
                 var giftCollectPacket = new XmasClientCollectGiftPacket();
                 NetManager.Instance.SendPacket(giftCollectPacket);
-                RunSuccess(immediate);
+                NetManager.Instance.OnPacket += ListenForPacket;
+                StartCoroutine(RunTimeout());
             }
             else {
                 RunNoConnection(immediate);
             }
         }
 
+        private IEnumerator RunTimeout() {
+            yield return new WaitForSeconds(TimeOutSeconds);
+            if (Sequence.CurrentAction == this) {
+                RunNoConnection(immediate);
+            }
+        }
+
+        private void ListenForPacket(XmasPacket packet) {
+            if (packet.PlayerID != uint.MaxValue)
+                return;
+            if (packet is XmasServerAcceptGiftPacket) {
+                RunSuccess(immediate);
+            }
+            else if (packet is XmasServerRejectGiftPacket) {
+                RunFailure(immediate);
+            }
+        }
+
         private void RunSuccess(bool immediate) {
+            NetManager.Instance.OnPacket -= ListenForPacket;
+            StopAllCoroutines();
+            if (Sequence.Sequence.Skippable)
+                CustomSequenceHandler.instance.skipTextActiveState = SequenceHandler.SkipState.IDLE;
             var progress = WinterProgress.Instance.LocalProgress;
             ToyLineManager.Instance.RespawnAllToyLines();
             progress.Gifts++;
@@ -41,6 +72,10 @@ namespace Winterland.Common {
         }
 
         private void RunFailure(bool immediate) {
+            NetManager.Instance.OnPacket -= ListenForPacket;
+            StopAllCoroutines();
+            if (Sequence.Sequence.Skippable)
+                CustomSequenceHandler.instance.skipTextActiveState = SequenceHandler.SkipState.IDLE;
             var action = Sequence.Sequence.GetActionByName(RejectedTarget);
             if (action != null)
                 action.Run(immediate);
@@ -49,6 +84,10 @@ namespace Winterland.Common {
         }
 
         private void RunNoConnection(bool immediate) {
+            NetManager.Instance.OnPacket -= ListenForPacket;
+            StopAllCoroutines();
+            if (Sequence.Sequence.Skippable)
+                CustomSequenceHandler.instance.skipTextActiveState = SequenceHandler.SkipState.IDLE;
             var action = Sequence.Sequence.GetActionByName(NoConnectionTarget);
             if (action != null)
                 action.Run(immediate);
