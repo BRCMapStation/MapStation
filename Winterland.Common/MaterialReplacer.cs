146148363ed429008e18e516deb4fc0c025fd9e7
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-22 06:45:05 -0300

    Replace billboard textures

--- Winterland.Common/MaterialReplacer.cs
+++ Winterland.Common/MaterialReplacer.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using CommonAPI;
 
 namespace Winterland.Common {
     public class MaterialReplacer : MonoBehaviour {
         public string NameOfMaterialToReplace = null;
         public Material ReplacementMaterial = null;
+        public bool UseBRCShader = false;
+        public AssetAPI.ShaderNames BRCShaderName = AssetAPI.ShaderNames.AmbientEnvironment;
 
-        private void Awake() {
+        private void Start() {
+            if (UseBRCShader)
+                ReplacementMaterial.shader = AssetAPI.GetShader(BRCShaderName);
             var renderers = FindObjectsOfType<Renderer>();
             foreach(var renderer in renderers) {
+                if (!renderer.sharedMaterials.Any((x) => (x != null && x.name == NameOfMaterialToReplace)))
+                    continue;
                 var newMaterials = new Material[renderer.sharedMaterials.Length];
                 for(var i = 0; i < renderer.sharedMaterials.Length; i++) {
                     var material = renderer.sharedMaterials[i];
-                    if (material.name == NameOfMaterialToReplace)
+                    if (material != null && material.name == NameOfMaterialToReplace)
                         newMaterials[i] = ReplacementMaterial;
                     else
                         newMaterials[i] = material;
