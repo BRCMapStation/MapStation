d82052075107b1e0c0b6fbc5a079e96cc8e1037b
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-22 17:14:22 -0300

    public toilet and cypher sorta working on challenge

---
+++ Winterland.Common/CustomToilet.cs
@@ -0,0 +1,21 @@
+using Reptile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Winterland.Common {
+    public class CustomToilet : MonoBehaviour {
+        private static GameObject Source = null;
+
+        private void Awake() {
+            if (Source == null) {
+                Source = FindObjectsOfType<PublicToilet>()[1].gameObject;
+            }
+            var myToilet = Instantiate(Source, transform);
+            myToilet.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+    }
+}
