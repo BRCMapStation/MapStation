ca351037b338db26fda3ec68d200aa52b5aac360
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-12-23 05:16:35 -0300

    workaround for santa not detecting all toy lines collected

--- Winterland.Common/ToyLineManager.cs
+++ Winterland.Common/ToyLineManager.cs
@@ -19,12 +19,17 @@ namespace Winterland.Common {
         }
 
         public bool GetCollectedAllToyLines() {
+            // Workaround for some kinda bug Pouch kept running into.
+            if (ToyLines.Count - WinterProgress.Instance.LocalProgress.ToyLinesCollected <= 0)
+                return true;
+            return false;
+            /*
             var progress = WinterProgress.Instance.LocalProgress;
             foreach(var toyLine in ToyLines) {
                 if (!progress.IsToyLineCollected(toyLine.Guid))
                     return false;
             }
-            return true;
+            return true;*/
         }
 
         public void RespawnAllToyLines() {
