f6576052129429d92bd015b39b7d97fd6edc7610
Andrew Bradley <cspotcode@gmail.com>
2023-12-25 18:56:24 -0500

    fix bug

--- Winterland.Common/Network/NetManager.cs
+++ Winterland.Common/Network/NetManager.cs
@@ -27,7 +27,11 @@ namespace Winterland.Common {
 
         private void Awake() {
             Instance = this;
-            this.PacketLogger = WinterLogging.CreateLogger(nameof(NetManager), onlyForDebugBuild: true, enabled: WinterConfig.Instance.LogPacketsValue);
+            bool enableLogger = false;
+#if WINTER_DEBUG
+            enableLogger = WinterConfig.Instance.LogPacketsValue;
+#endif
+            this.PacketLogger = WinterLogging.CreateLogger(nameof(NetManager), onlyForDebugBuild: true, enabled: enableLogger);
             
             APIManager.OnAPIRegistered += OnSlopCrewAPIRegistered;
             var api = APIManager.API;
