a185fa1923ea4a2fe1c0f3856a4e89beb6b4e2f9
Andrew Bradley <cspotcode@gmail.com>
2024-01-05 19:44:51 -0500

    cleanup

--- MapStation.Tools/Assets/Scripts/Editor/BuildConstants.cs
+++ MapStation.Tools/Assets/Scripts/Editor/BuildConstants.cs
@@ -3,8 +3,8 @@ namespace MapStation.Tools {
         public static string BuiltBundlesDirectory(bool compressed) {
             return compressed ? BuiltCompressedDirectory : BuiltUncompressedDirectory;
         }
-        public const string BuiltUncompressedDirectory = "Assets/Temp/Bundles";
-        public const string BuiltCompressedDirectory = "Assets/Temp/CompressedBundles";
+        public const string BuiltUncompressedDirectory = "Assets/Temp/Uncompressed";
+        public const string BuiltCompressedDirectory = "Assets/Temp/Compressed";
         public const string BuiltZipFilename = "map" + Common.PathConstants.MapFileExtension;
         public const string PluginName = "MapStation";
     }
