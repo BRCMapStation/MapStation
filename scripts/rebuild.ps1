4b7e5fb086e645fd075f1f204ceaa15380256942
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-01-06 18:09:47 -0300

    fix rebuild trying to checkout to assets/scripts

--- scripts/rebuild.ps1
+++ scripts/rebuild.ps1
@@ -3,13 +3,13 @@ Write-Output "====== Updating Submodules ======="
 git submodule update --init --recursive
 Write-Output "====== Restoring Assembly GUIDs ======="
 
-$metaPath = ".\MapStation.Editor\Assets\Scripts\EasyDecal.Runtime.dll.meta"
+$metaPath = ".\MapStation.Editor\Assets\EasyDecal.Runtime.dll.meta"
 git checkout $metaPath
 
-$metaPath = ".\MapStation.Editor\Assets\Scripts\CommonAPI.dll.meta"
+$metaPath = ".\MapStation.Editor\Assets\CommonAPI.dll.meta"
 git checkout $metaPath
 
-$metaPath = ".\MapStation.Editor\Assets\Scripts\SlopCrew.API.dll.meta"
+$metaPath = ".\MapStation.Editor\Assets\SlopCrew.API.dll.meta"
 git checkout $metaPath
 
 Write-Output "====== Building Assemblies ======="
