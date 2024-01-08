2f51bf3ed869cced0c2b59e3e3c3204915126ac9
Andrew Bradley <cspotcode@gmail.com>
2024-01-08 00:31:59 -0500

    add KbmInputDisabler from winterland

---
+++ MapStation.Plugin/Patches/PlayerPatch.cs
@@ -0,0 +1,16 @@
+﻿using Reptile;
+using HarmonyLib;
+using Winterland.Plugin;
+
+namespace MapStation.Plugin.Patches;
+
+[HarmonyPatch(typeof(Player))]
+internal static class PlayerPatch {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Player.Init))]
+    private static void Init_Postfix(Player __instance) {
+        if(MapStationConfig.Instance.DisableKBMInputValue) {
+            KBMInputDisabler.Disable();
+        }
+    }
+}
