95bea271867bd908a58976729b23e52c6c50aea6
Andrew Bradley <cspotcode@gmail.com>
2024-01-12 23:39:53 -0500

    tweak copy-assets to avoid changing cwd in interactive shell

--- scripts/copy-assets.ps1
+++ scripts/copy-assets.ps1
@@ -1,4 +1,9 @@
-cd ../
-$bepinexDirectory = $env:BepInExDirectory
-$destinationPath = "$bepinexDirectory\plugins\MapStation"
-Copy-Item -Path "Assets\*" -Destination $destinationPath -Recurse
+$RestoreCwd = Get-Location
+Set-Location $PSScriptRoot/..
+try {
+    $bepinexDirectory = $env:BepInExDirectory
+    $destinationPath = "$bepinexDirectory\plugins\MapStation"
+    Copy-Item -Path "Assets\*" -Destination $destinationPath -Recurse
+} finally {
+    Set-Location $RestoreCwd
+}
