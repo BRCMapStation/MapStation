ba6e4775303368500661eeda01f9e27065f858f8
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-01-12 22:50:01 -0300

    make map station app title font a little smaller

--- MapStation.Plugin/Phone/AppMapStation.cs
+++ MapStation.Plugin/Phone/AppMapStation.cs
@@ -10,7 +10,7 @@ namespace MapStation.Plugin.Phone {
     public class AppMapStation : CustomApp {
         public override void OnAppInit() {
             base.OnAppInit();
-            CreateIconlessTitleBar("MapStation");
+            CreateIconlessTitleBar("MapStation", 70f);
             ScrollView = PhoneScrollView.Create(this);
         }
 
