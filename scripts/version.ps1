4cc9ce73d22505b086ca5267ce51df0dc7b4df52
Andrew Bradley <cspotcode@gmail.com>
2024-01-12 20:30:23 -0500

    add versioning script

---
+++ scripts/version.ps1
@@ -0,0 +1,69 @@
+param(
+    [switch]$major,
+    [switch]$minor,
+    [switch]$patch,
+    [string]$version,
+    [switch]$NoGit
+)
+
+#Requires -Version 7.4
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+function PatchJson($path, $fn) {
+    $json = Get-Content $path | ConvertFrom-Json
+    # Trick to call $fn and set the automatic $_ var
+    ForEach-Object -Process $fn -InputObject $json
+    $json | ConvertTo-Json | Set-Content $path
+}
+
+$RestoreCwd = Get-Location
+Set-Location $PSScriptRoot/..
+try {
+
+    # Abort if git shows uncommitted changes
+    if("" -ne $(git status --untracked-files=no --porcelain=v1)) {
+        Write-Error "Git status shows modified files. This script cannot commit a new version while there are uncommitted, modified files."
+    }
+
+    # Delegate to npm for versioning logic, it's easier
+    $npmFlags = @(
+        if($major) {'major'}
+        if($minor) {'minor'}
+        if($patch) {'patch'}
+        if($version) {$version}
+    )
+    if($npmFlags.Count -eq 0) {
+        Write-Error 'Missing flags'
+    }
+    npm version --no-git-tag-version @npmFlags
+
+    # Copy version number to other files
+    $version = (Get-Content package.json | ConvertFrom-Json).version
+
+    PatchJson './MapStation.Common/package.json' {
+        $_.version = $version
+    }
+    PatchJson './MapStation.Tools/package.json' {
+        $_.dependencies.'com.brcmapstation.common' = $version
+        $_.version = $version
+    }
+    PatchJson './thunderstore/manifest.json' {
+        $_.version_number = $version
+    }
+
+    if(-not $NoGit) {
+        git add `
+            './package.json' `
+            './MapStation.Common/package.json' `
+            './MapStation.Tools/package.json' `
+            './thunderstore/manifest.json'
+        git commit -m "v$version"
+        git tag $version
+
+        Write-Host -ForegroundColor Green "Don't forget to 'git push --tags'"
+    }
+
+} finally {
+    Set-Location $RestoreCwd
+}
