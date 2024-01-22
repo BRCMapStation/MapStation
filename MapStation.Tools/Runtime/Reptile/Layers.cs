5f6af525cd0e3620709b06181ebe971ac0d8b551
Andrew Bradley <cspotcode@gmail.com>
2024-01-21 20:24:35 -0500

    add missing Reptile.Layers enum

---
+++ MapStation.Tools/Runtime/Reptile/Layers.cs
@@ -0,0 +1,63 @@
+﻿namespace Reptile
+{
+    public static class Layers
+    {
+        public const int Default = 0;
+
+        public const int TransparentFX = 1;
+
+        public const int IgnoreRaycast = 2;
+
+        public const int Water = 4;
+
+        public const int UI = 5;
+
+        public const int Sun = 8;
+
+        public const int PlayerInteract = 9;
+
+        public const int NonStableSurface = 10;
+
+        public const int Grinding = 11;
+
+        public const int CameraIgnore = 12;
+
+        public const int Wallrun = 13;
+
+        public const int TriggerDetectDefault = 14;
+
+        public const int Player = 15;
+
+        public const int Pickups = 16;
+
+        public const int Enemies = 17;
+
+        public const int PlayerHitbox = 18;
+
+        public const int TriggerDetectPlayer = 19;
+
+        public const int EnemyHitbox = 20;
+
+        public const int Junk = 21;
+
+        public const int DetectObstructions = 22;
+
+        public const int VertSurface = 23;
+
+        public const int Phone = 24;
+
+        public const int StreetLife = 25;
+
+        public const int DetectGrind = 26;
+
+        public const int Minimap = 27;
+
+        public const int Projectile = 28;
+
+        public const int NonCollidingJunk = 29;
+
+        public const int TriggerDetectDynamic = 30;
+
+        public const int DontUseThisLayer = 31;
+    }
+}
