d58a5fa6e9d0b199e12579ebc773d6bc02a7ba9a
Andrew Bradley <cspotcode@gmail.com>
2024-02-09 19:01:05 -0500

    fix

--- MapStation.Common/Runtime/Components/DeleteVanillaGameObjects.cs
+++ MapStation.Common/Runtime/Components/DeleteVanillaGameObjects.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using MapStation.Common.Serialization;
+using Reptile;
 
 namespace MapStation.Common.VanillaAssets {
     /// <summary>
