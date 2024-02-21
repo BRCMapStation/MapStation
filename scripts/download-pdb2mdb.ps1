c1b714f38b648003927025b5933ebc2f97039234
cspotcode <cspotcode@gmail.com>
2024-02-21 17:56:46 -0500

    .mdb generation improvements: - Make .mdb generation generic, applied automatically to all projects in the solution - add script to download pdb2mdb - add conditional logic so building projects directly, without solution context, succeeds

---
+++ scripts/download-pdb2mdb.ps1
@@ -0,0 +1,7 @@
+﻿# Download pdb2mdb.exe from this gist:
+# https://gist.github.com/jbevain/ba23149da8369e4a966f#file-pdb2mdb-exe
+# ...which is recommended by BepInEx docs:
+# https://github.com/BepInEx/bepinex-docs/blob/master/articles/advanced/debug/plugins_vs.md/#converting-pdb-to-mdb
+# pdb2mdb.exe is bundled with Unity, but it doesn't work without extra tricks to force it to run under Unity's mono VM (??)
+
+Invoke-WebRequest -OutFile $PSScriptRoot/pdb2mdb.exe https://gist.github.com/jbevain/ba23149da8369e4a966f/raw/36b3cdd4dd149ab966bbb48141ef8ee2d37c890f/pdb2mdb.exe
