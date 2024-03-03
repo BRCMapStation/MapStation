d3ee838d87f04d6aef410cc40db3dfe9c79f930a
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-03-03 16:41:03 -0300

    Add AmbientTrigger script

--- MapStation.Tools/Runtime/Reptile/AmbientTrigger.cs
+++ MapStation.Tools/Runtime/Reptile/AmbientTrigger.cs
@@ -4,5 +4,12 @@ namespace Reptile
 {
 	public class AmbientTrigger : MonoBehaviour
 	{
-	}
+        public Color AmbientEnvLight;
+
+        public Color AmbientEnvShadow;
+
+        public float transitionDuration = 2f;
+
+        public float revertTransitionDuration = 2f;
+    }
 }
