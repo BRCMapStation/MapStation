eb0287ef42641d4ce4cd240fde3d56e0e409b9d5
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-09-06 12:16:31 -0300

    replace door and stuff in toilet proxy

--- MapStation.Common/Runtime/Gameplay/MapStationPublicToilet.cs
+++ MapStation.Common/Runtime/Gameplay/MapStationPublicToilet.cs
@@ -8,9 +8,26 @@ using UnityEngine;
 
 namespace MapStation.Common.Runtime.Gameplay {
     public class MapStationPublicToilet : MonoBehaviour {
+        public Transform Exit;
+        public Animator DoorAnimator;
+        public GameObject GreenLight;
+        public GameObject RedLight;
         private void Awake() {
 #if BEPINEX
             var myToilet = StagePrefabHijacker.Prefabs.GetToilet();
+            var toiletScript = myToilet.GetComponent<PublicToilet>();
+            Destroy(toiletScript.doorAnimator.gameObject);
+
+            toiletScript.exit = Exit;
+            toiletScript.doorAnimator = DoorAnimator;
+            toiletScript.greenLight = GreenLight;
+            toiletScript.redLight = RedLight;
+            foreach(var output in toiletScript.sequence.playableAsset.outputs) {
+                if (output.streamName == "Animation Track (3)") {
+                    toiletScript.sequence.SetGenericBinding(output.sourceObject, DoorAnimator);
+                    break;
+                }
+            }
             myToilet.transform.SetParent(transform, false);
             myToilet.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
 #endif
