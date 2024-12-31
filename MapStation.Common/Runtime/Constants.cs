1adc35d9979f16252a613f9793b2cb7585be728f
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-12-31 14:52:25 -0300

    Set MinTriggerRadius for grinds to 0

--- MapStation.Common/Runtime/Constants.cs
+++ MapStation.Common/Runtime/Constants.cs
@@ -3,7 +3,7 @@ using System.IO;
 namespace MapStation.Common {
 
     public class GrindConstants {
-        public const float MinTriggerRadius = 0.1f;
+        public const float MinTriggerRadius = 0.0f;
         public const float MaxTriggerRadius = 0.5f;
         public const float DefaultTriggerRadius = 0.28f;
     }
