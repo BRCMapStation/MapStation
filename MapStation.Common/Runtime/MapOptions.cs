06007d96d67981da9e0bce2ebaebe6957ae2f4c7
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-12-31 00:48:35 -0300

    Add Map Options component

---
+++ MapStation.Common/Runtime/MapOptions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MapStation.Common.Runtime {
+    public class MapOptions : MonoBehaviour {
+        public MapOption[] Options;
+
+        [Serializable]
+        public class MapOption {
+            public string ID;
+            public string Name;
+            [TextArea(5,5)]
+            public string Description;
+            public string DefaultValue;
+            public string[] PossibleValues;
+            public Camera PreviewCamera;
+        }
+    }
+}
