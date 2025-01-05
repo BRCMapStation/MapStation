2c96c495e8941fe41ee9f91e374670861c3d73b7
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-01-05 18:47:15 -0300

    Add lighting changer

---
+++ MapStation.Common/Runtime/LightingChanger.cs
@@ -0,0 +1,77 @@
+using Reptile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MapStation.Common.Runtime {
+    public class LightingChanger : MonoBehaviour {
+        [Header("This GameObject's Transform will be applied to the Sun.")]
+        [Header("Light Settings")]
+        public Color LightColor = Color.white;
+        [Header("BRC Shading Settings")]
+        public Color EnvLightColor = Color.white;
+        public Color EnvShadowColor = Color.black;
+
+        private static Quaternion OldRotation = Quaternion.identity;
+        private static Color OldLightColor = Color.white;
+        private static Color OldEnvLightColor = Color.white;
+        private static Color OldEnvShadowColor = Color.black;
+
+        private static LightingChanger CurrentLightingChanger = null;
+
+#if BEPINEX
+        private void Awake() {
+            StageManager.OnStageInitialized += StageInitialized;
+        }
+
+        private void OnDestroy() {
+            StageManager.OnStageInitialized -= StageInitialized;
+        }
+
+        private void StageInitialized() {
+            if (isActiveAndEnabled)
+                OnEnable();
+        }
+
+        private AmbientManager GetAmbientManager() {
+            AmbientManager ambientManager = null;
+            var ambientManagers = FindObjectsOfType<AmbientManager>();
+            foreach (var manager in ambientManagers) {
+                if (manager.gameObject.scene == gameObject.scene) {
+                    ambientManager = manager;
+                    break;
+                }
+            }
+            return ambientManager;
+        }
+
+        private void OnEnable() {
+            var ambientManager = GetAmbientManager();
+            if (CurrentLightingChanger == null) {
+                OldRotation = ambientManager.transform.rotation;
+                OldLightColor = ambientManager.GetComponent<Light>().color;
+                OldEnvLightColor = ambientManager.AmbientEnvLight;
+                OldEnvShadowColor = ambientManager.AmbientEnvShadow;
+            }
+            CurrentLightingChanger = this;
+            ambientManager.GetComponent<Light>().color = LightColor;
+            ambientManager.transform.rotation = transform.rotation;
+            ambientManager.AmbientEnvLight = EnvLightColor;
+            ambientManager.AmbientEnvShadow = EnvShadowColor;
+        }
+
+        private void OnDisable() {
+            if (CurrentLightingChanger != this) return;
+            CurrentLightingChanger = null;
+            var ambientManager = GetAmbientManager();
+            ambientManager.GetComponent<Light>().color = OldLightColor;
+            ambientManager.transform.rotation = OldRotation;
+            ambientManager.AmbientEnvLight = OldEnvLightColor;
+            ambientManager.AmbientEnvShadow = OldEnvShadowColor;
+        }
+#endif
+    }
+}
